1c06f5b830a29bb7ea061b30242b665d08e03acd
bichvan278 <mintmint27899@gmail.com>
2021-06-28 09:56:58 +0700

    layout doi mat khau

--- app/src/main/java/com/example/chamcong/Trangchu.java
+++ app/src/main/java/com/example/chamcong/Trangchu.java
@@ -2,13 +2,24 @@ package com.example.chamcong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Trangchu extends AppCompatActivity {
-
+    Button scanBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trangchu);
+        scanBtn = findViewById(R.id.scanBtn);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),ScanQR.class));
+            }
+        });
     }
 }
