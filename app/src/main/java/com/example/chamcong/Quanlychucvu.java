79633022252b8f0da0c96c577e6e1f1c8bdcd430
bichvan278 <mintmint27899@gmail.com>
2021-07-04 00:13:35 +0700

    Thêm các layout mới

---
+++ app/src/main/java/com/example/chamcong/Quanlychucvu.java
@@ -0,0 +1,14 @@
+package com.example.chamcong;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Quanlychucvu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quanlychucvu);
+    }
+}
