1e305e4a9ccb790397c605e87d993c26e4b52eef
lanbp31 <buiphuonglan.bpl@gmail.com>
2021-07-05 07:35:19 +0700

    them ConnectionHelper

---
+++ app/src/main/java/com/example/chamcong/ConnectionHelper.java
@@ -0,0 +1,41 @@
+package com.example.chamcong;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+    @SuppressLint("NewApi")
+    public static Connection CONN() {
+
+        String username = "";
+        String password = "";
+        String db = "";
+        String ip = "";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName= " + db
+                    + ";user= " + username
+                    + ";password= " + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
+
