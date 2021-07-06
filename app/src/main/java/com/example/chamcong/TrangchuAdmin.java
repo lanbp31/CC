f297501da329292988042cd0d023d50d27890410
lanbp31 <buiphuonglan.bpl@gmail.com>
2021-07-06 16:59:08 +0700

    act và xml TrangchuAdmin

--- app/src/main/java/com/example/chamcong/TrangchuAdmin.java
+++ app/src/main/java/com/example/chamcong/TrangchuAdmin.java
@@ -2,13 +2,46 @@ package com.example.chamcong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrangchuAdmin extends AppCompatActivity {
+
+    Button scanBtn, logoutBtn, historyBtn, infoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trangchu_admin);
+        setContentView(R.layout.activity_trangchu);
+        scanBtn = findViewById(R.id.scanBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
+        historyBtn = findViewById(R.id.historyBtn);
+        infoBtn = findViewById(R.id.infoBtn);
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TrangchuAdmin.this, ScanQR.class);
+                startActivity(i);
+            }
+        });
+
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TrangchuAdmin.this, Lichsuchamcong.class);
+                startActivity(i);
+            }
+        });
+
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TrangchuAdmin.this, Thongtinuser.class);
+                startActivity(i);
+            }
+        });
     }
 }
