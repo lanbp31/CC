aef7c47975943b10981d4d46af18e93d50ea2e37
Admin <bichvan287>
2021-07-09 23:42:23 +0700

    Chỉnh sửa định dạng layout

--- app/src/main/java/com/example/chamcong/ThongKeNgayFragment.java
+++ app/src/main/java/com/example/chamcong/ThongKeNgayFragment.java
@@ -7,12 +7,13 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+
 public class ThongKeNgayFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thong_ke_ngay, container, false);
+        return inflater.inflate(R.layout.fragment_thong_ke_thang, container, false);
     }
 }
--- app/src/main/java/com/example/chamcong/Thongtinuser.java
+++ app/src/main/java/com/example/chamcong/Thongtinuser.java
@@ -1,14 +1,83 @@
 package com.example.chamcong;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Thongtinuser extends AppCompatActivity {
 
+    Connection connect;
+    EditText hoten, sdt, gioitinh, email, diachi, phuong, quan, tp;
+    TextView chucvu, bophan;
+    Button btnnewpw, btnSave;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongtinuser);
+
+        hoten = (EditText) findViewById(R.id.hoten);
+        sdt = (EditText) findViewById(R.id.sdt);
+        gioitinh = (EditText) findViewById(R.id.gioitinh);
+        email = (EditText) findViewById(R.id.email);
+        diachi = (EditText) findViewById(R.id.diachi);
+        phuong = (EditText) findViewById(R.id.phuong);
+        quan = (EditText) findViewById(R.id.quan);
+        tp = (EditText) findViewById(R.id.tp);
+
+        chucvu = (TextView) findViewById(R.id.chucvu);
+        bophan = (TextView) findViewById(R.id.bophan);
+
+        btnnewpw = (Button) findViewById(R.id.btnnewpw);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ht, dt, gt, eml, dc, phg, qun, tpho;
+                ht = hoten.getText().toString();
+                dt = sdt.getText().toString();
+                gt = gioitinh.getText().toString();
+                eml = email.getText().toString();
+                phg = phuong.getText().toString();
+                qun = quan.getText().toString();
+                tpho = tp.getText().toString();
+
+                ConnectionHelper conn = new ConnectionHelper();
+            }
+        });
+
+        btnnewpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void insertdata ( String u_hoten, String u_sdt, String u_gioitinh, String u_email, String u_phuong, String u_quan, String u_tp){
+        try {
+            ConnectionHelper conHelper = new ConnectionHelper();
+            connect = conHelper.CONN();
+            if(connect != null){
+                String query = "INSERT INTO users VALUES ('" + u_hoten + "','" + u_sdt + "','" + u_gioitinh + "','" + u_email + "','" + u_phuong + "','"+ u_quan +"'" +
+                        ",'"+ u_tp +"');";
+                Statement st = connect.createStatement();
+                ResultSet result = st.executeQuery(query);
+            }
+        }
+        catch (Exception e){
+
+        }
     }
 }
